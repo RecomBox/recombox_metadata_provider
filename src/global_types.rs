4e7004db7aece6aa4598c0b074114e2df617016f
unknown <istartgame31@gmail.com>
2026-03-30 22:24:44 +0700

    fix global type

--- src/global_types.rs
+++ src/global_types.rs
@@ -12,4 +12,13 @@ impl Source {
             Source::TV => String::from("tv")
         }
     }
+
+    pub fn from_str(s: &str) -> Source {
+        match s.to_lowercase().as_str() {
+            "anime" => Source::Anime,
+            "movies" => Source::Movies,
+            "tv" => Source::TV,
+            _ => Source::Anime
+        }
+    }
 }
